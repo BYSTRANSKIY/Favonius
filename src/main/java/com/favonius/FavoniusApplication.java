e8ce1a050ff142aabe438097938eab8d85205069
Misha Bystranskyi <mbystranskiy@gmail.com>
2023-07-23 13:15:24 +0300

    init project

---
+++ src/main/java/com/favonius/FavoniusApplication.java
@@ -0,0 +1,13 @@
+package com.favonius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoniusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavoniusApplication.class, args);
+    }
+
+}
